a3edaea21d938f44cfa686277007dd732e6edbc3
Annabelle <annabelleellis1999@gmail.com>
2018-11-23 09:36:12 -0500

    Second Commit

--- maze.js
+++ maze.js
@@ -1,37 +1,65 @@
-function setUpEvents(){
+function changeRed(topLeft, display,start){
+	//topLeft.setAttribute("class","boundary youloose");
+	// topLeft.classList.add("youlose");
+	// topLeft.className += " youloose";
+	console.log(topLeft);
+	for(var i = 0; i < topLeft.length;i++){
+		console.log(topLeft[i]);
+		console.log(topLeft[i].classList);
+		//Why does theline above print out the class list but 
+		//also gives an error?
+		topLeft[i].addEventListener("mouseover",function(){
+			// this.classList.add("youlose");
 
-}
-function changeBackground(){
-	var backColor = document.getElementsByClassName("boundary");
-
-	for(var i = 0; i < backColor.length - 1; i++ ){
-		backColor[i].classList.add('youlose');
-		//style.backgroundColor = 'red';
-
+			for (var x = 0; x < topLeft.length; x++) {
+				topLeft[x].classList.add("youlose");
+			}
+			console.log(this.classList);
+		},false);
+		// display.innerHTML ="You LOSE!!!";
 	}
-}
-
-/*function backToNormal(){
-	var backColor = document.getElementsByClassName("boundary");
-
-	for(var i = 0; i < backColor.length - 1; i++ ){
-		backColor[i]..classList.remove('youlose');
-		//style.backgroundColor = '';
-
-	}
-}*/
-
-function win(){
-	//console.log(winner);
-	//var winner = document.getElementById('end');
-	var winner = document.querySelector('#maze');
-	winner.innerHTML = ('<h2>You WIN</h2>');
+	// display.innerHTML ="You LOSE!!!";
 
 }
 
+function gameEnd(display){
+	var end = document.querySelector("#end");
+	end.addEventListener("mouseover", function(){
+		display.innerHTML = "YOU WIN!!!";
+	});
+}
 
-	
-	
+function gameStart(display){
+	start = true;
+	var start = document.getElementById('start');
+	start.addEventListener("click",function(){
+		display.innerHTML ="The Game Has Begun";
+	});
+}
+
+function gameCheater(topLeft){
+	topLeft.addEventListener("mouseout",function(){
+		display.innerHTML ="You Lose";
+	})
+
+}
+
 window.onload = function(){
-	setUpEvents()
+
+	// var topLeft = document.getElementsByClassName('boundary');
+	//console.log(topLeft);
+	// topLeft.addEventListener("mouseover",changeRed, false);
+	// topLeft.onmouseover = function{changeRed(topLeft)};
+	var start = false;
+	var topLeft = document.querySelectorAll('#maze .boundary');
+	var display = document.getElementById("status");
+
+	if (!start){
+		gameStart(display);
+		changeRed(topLeft, display,start);
+		gameEnd(display);
+	}
+
+
+
 }
